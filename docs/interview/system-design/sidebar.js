02b59037aeb74969390eba1dcb14ca50be29d74a
wxning <wxning2020@gmail.com>
2021-10-24 19:46:29 +0800

    edit theme

--- docs/interview/system-design/sidebar.js
+++ docs/interview/system-design/sidebar.js
@@ -3,7 +3,6 @@ module.exports = [
         title: '基础知识',
         collapsable: false,
         children: [
-            '/interview/system-design/notes/01/',
             '/interview/system-design/notes/01/01.md',
             '/interview/system-design/notes/01/02.md',
             '/interview/system-design/notes/01/03.md',
@@ -13,7 +12,6 @@ module.exports = [
         title: '数据库设计',
         collapsable: false,
         children: [
-            '/interview/system-design/notes/02/',
             '/interview/system-design/notes/02/01.md',
             '/interview/system-design/notes/02/02.md',
             '/interview/system-design/notes/02/03.md',
@@ -25,7 +23,6 @@ module.exports = [
         title: '缓存设计',
         collapsable: false,
         children: [
-            '/interview/system-design/notes/03/',
             '/interview/system-design/notes/03/01.md',
             '/interview/system-design/notes/03/02.md',
             '/interview/system-design/notes/03/03.md',
