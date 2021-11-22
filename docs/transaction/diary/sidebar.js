7d159ec9dcc8d08bcc7a922b30341d611c774f63
wxning <wxning2020@gmail.com>
2021-11-22 18:19:54 +0800

    add transaction diary

--- docs/transaction/diary/sidebar.js
+++ docs/transaction/diary/sidebar.js
@@ -3,6 +3,9 @@ module.exports = [
         title: '2021',
         collapsable: false,
         children: [
+            '/transaction/diary/notes/00/04.md',
+            '/transaction/diary/notes/00/03.md',
+            '/transaction/diary/notes/00/02.md',
             '/transaction/diary/notes/00/01.md',
         ]
     },
