19a3d399efbc63d6338ea042a4cf718b05f195d2
wxning <wxning2020@gmail.com>
2021-11-23 10:17:10 +0800

    add redis

--- docs/interview/redis/sidebar.js
+++ docs/interview/redis/sidebar.js
@@ -1,12 +1,13 @@
 module.exports = [
     {
-        title: '基础知识',
+        title: '基础篇',
         collapsable: false,
         children: [
             '/interview/redis/notes/00/01.md',
             '/interview/redis/notes/00/02.md',
             '/interview/redis/notes/00/03.md',
             '/interview/redis/notes/00/04.md',
+            '/interview/redis/notes/00/05.md',
         ]
     },
 ]
