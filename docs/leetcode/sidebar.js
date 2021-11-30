35880acff7dd902b29d46f99fd62317ac7e2ae85
wxning <wxning2020@gmail.com>
2021-11-30 20:02:23 +0800

    abc leetcode

--- docs/leetcode/sidebar.js
+++ docs/leetcode/sidebar.js
@@ -1,6 +1,14 @@
 module.exports = [
     {
-        title: 'LeetCode Algorithm',
+        title: 'Dynamic Programming',
+        collapsable: false,
+        children: [
+            '/leetcode/notes/00/01.md',
+            '/leetcode/notes/00/02.md',
+        ]
+    },
+    {
+        title: 'Array',
         collapsable: false,
         children: [
             '/leetcode/notes/00/01.md',
