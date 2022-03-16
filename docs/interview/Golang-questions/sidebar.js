ad89ee0c37443962b99229bdb66160a0f989896e
wxning <wxning2020@gmail.com>
2022-03-16 17:08:39 +0800

    add golang

--- docs/interview/Golang-questions/sidebar.js
+++ docs/interview/Golang-questions/sidebar.js
@@ -1,9 +1,31 @@
 module.exports = [
     {
-        title: '基础篇',
+        title: '',
         collapsable: false,
         children: [
             '/interview/Golang-questions/notes/00/01.md',
+            '/interview/Golang-questions/notes/00/02.md',
+            '/interview/Golang-questions/notes/00/03.md',
+            '/interview/Golang-questions/notes/00/04.md',
+            '/interview/Golang-questions/notes/00/05.md',
+            '/interview/Golang-questions/notes/00/06.md',
+            '/interview/Golang-questions/notes/00/07.md',
+            '/interview/Golang-questions/notes/00/08.md',
+            '/interview/Golang-questions/notes/00/09.md',
+            '/interview/Golang-questions/notes/00/10.md',
+            '/interview/Golang-questions/notes/00/11.md',
+            '/interview/Golang-questions/notes/00/12.md',
+            '/interview/Golang-questions/notes/00/13.md',
+            '/interview/Golang-questions/notes/00/14.md',
+            '/interview/Golang-questions/notes/00/15.md',
+            '/interview/Golang-questions/notes/00/16.md',
+            '/interview/Golang-questions/notes/00/17.md',
+            '/interview/Golang-questions/notes/00/18.md',
+            '/interview/Golang-questions/notes/00/19.md',
+            '/interview/Golang-questions/notes/00/20.md',
+            '/interview/Golang-questions/notes/00/21.md',
+            '/interview/Golang-questions/notes/00/22.md',
+            '/interview/Golang-questions/notes/00/23.md',
         ]
     },
 ]
