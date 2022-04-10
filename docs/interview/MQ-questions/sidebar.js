e07c97f925be519665500e013b5f60275cdd969f
wxning <wxning2020@gmail.com>
2022-04-10 09:44:52 +0800

    add kafka

--- docs/interview/MQ-questions/sidebar.js
+++ docs/interview/MQ-questions/sidebar.js
@@ -5,8 +5,6 @@ module.exports = [
         children: [
             '/interview/MQ-questions/notes/00/01.md',
             '/interview/MQ-questions/notes/00/02.md',
-            '/interview/MQ-questions/notes/00/04.md',
-            '/interview/MQ-questions/notes/00/05.md',
             '/interview/MQ-questions/notes/00/06.md',
             '/interview/MQ-questions/notes/00/08.md',
             '/interview/MQ-questions/notes/00/09.md',
