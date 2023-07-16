5c00fde21d5fe70824a10f2963e4a347b6b6518b
elon.wang <elon.wang@bybit.com>
2023-07-16 22:21:30 +0800

    add english

--- docs/english/spoken/sidebar.js
+++ docs/english/spoken/sidebar.js
@@ -9,14 +9,24 @@ module.exports = [
             '/english/spoken/notes/00/04.md',
         ]
     },
-    // {
-    //     title: '口语笔记',
-    //     collapsable: false,
-    //     children: [
-    //         '/english/diary/spoken/01/01.md',
-    //         '/english/diary/spoken/01/02.md',
-    //         '/english/diary/spoken/01/03.md',
-    //         '/english/diary/spoken/01/04.md',
-    //     ]
-    // },
+    {
+        title: 'Bob 英语',
+        collapsable: false,
+        children: [
+            '/english/diary/spoken/01/01.md',
+            '/english/diary/spoken/01/02.md',
+            '/english/diary/spoken/01/03.md',
+            '/english/diary/spoken/01/04.md',
+        ]
+    },
+    {
+        title: 'No stupid questions',
+        collapsable: false,
+        children: [
+            '/english/diary/spoken/no_stupid_questions/01.md',
+            '/english/diary/spoken/no_stupid_questions/02.md',
+            '/english/diary/spoken/no_stupid_questions/03.md',
+            '/english/diary/spoken/no_stupid_questions/04.md',
+        ]
+    },
 ]
